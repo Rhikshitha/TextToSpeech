e178e3aa8274e2e1088c5c816a544a4943fdc7d7
Rhikshitha <rhikshithakannan@gmail.com>
2023-04-01 09:16:35 +0530

    FINAL COMMIT

--- app/src/main/java/com/example/texttospeech/MainActivity.kt
+++ app/src/main/java/com/example/texttospeech/MainActivity.kt
@@ -5,19 +5,36 @@ import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.EditText
+import java.util.*
 
 class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener{
     lateinit var etText: EditText
     lateinit var  btSpeak: Button
-
+    lateinit var tts: TextToSpeech
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etText = findViewById(R.id.etText)
+        btSpeak=findViewById(R.id.btSpeak)
+        tts= TextToSpeech(this,this)
+        btSpeak.setOnClickListener {
+            val text =etText.text.toString()
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH,null,"")
+
+        }
+
     }
 
-    override fun onInit(p0: Int) {
-        TODO("Not yet implemented")
+    override fun onInit(status: Int) {
+        if(status==TextToSpeech.SUCCESS) {
+            var result = tts.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+
+            } else {
+                btSpeak.isEnabled = true
+            }
+        }
     }
 }
